a33003cce447d834bab0e103f06a65203761c7cb
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-07 18:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function stringChop(str, size) {
   // your code here
+	const charCount = {};
+
+	
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+ 
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  
+  return null;
+
 }
 
 // Do not change the code below
